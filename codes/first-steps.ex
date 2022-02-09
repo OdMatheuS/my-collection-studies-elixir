a2f61582d446ced8d673a437edba24543f114cab
Matheus <matheusnagot@hotmail.com>
2022-02-09 20:05:53 -0300

    removing unnecessary signatures

--- codes/first-steps.ex
+++ codes/first-steps.ex
@@ -1,28 +1,23 @@
 defmodule Lasagna do
-
   @spec expected_minutes_in_oven() :: integer
   def expected_minutes_in_oven() do
     40
   end
 
-
-  @spec remaining_minutes_in_oven(a :: integer()) :: integer()
+  @spec remaining_minutes_in_oven(x :: integer) :: integer
   def remaining_minutes_in_oven(x) do
     expected_minutes_in_oven() - x
   end
 
-
-  @spec preparation_time_in_minutes(x :: integer()) :: integer()
+  @spec preparation_time_in_minutes(x :: integer) :: integer
   def preparation_time_in_minutes(x) do
     x * 2
   end
 
-
-  @spec total_time_in_minutes(integer(), integer()) :: integer()
+  @spec total_time_in_minutes(x :: integer, y :: integer) :: integer
   def total_time_in_minutes(x, y) do
     preparation_time_in_minutes(x) + y
   end
-
 
   @spec alarm() :: String.t()
   def alarm() do
--- codes/pacman-rules.ex
+++ codes/pacman-rules.ex
@@ -5,25 +5,25 @@ defmodule Rules do
   @type touching_dot :: boolean
   @type has_eaten_all_dots :: boolean
 
-  @spec eat_ghost?() :: boolean()
+  @spec eat_ghost?() :: boolean
   def eat_ghost?(power_pellet_active, touching_ghost) do
     # Please implement the eat_ghost?/2 function
     power_pellet_active and touching_ghost
   end
 
-  @spec score?() :: boolean()
+  @spec score?() :: boolean
   def score?(touching_power_pellet, touching_dot) do
     # Please implement the score?/2 function
     touching_power_pellet or touching_dot
   end
 
-  @spec lose?() :: boolean()
+  @spec lose?() :: boolean
   def lose?(power_pellet_active, touching_ghost) do
     # Please implement the lose?/2 function
     not power_pellet_active and touching_ghost
   end
 
-  @spec win?() :: boolean()
+  @spec win?() :: boolean
   def win?(has_eaten_all_dots, power_pellet_active, touching_ghost) do
     # Please implement the win?/3 function
     has_eaten_all_dots and not Rules.lose?(power_pellet_active, touching_ghost)
