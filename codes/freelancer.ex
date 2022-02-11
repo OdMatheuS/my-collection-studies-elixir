1580b15bcc4a977650232510c44183bb3bbed3bd
Matheus <matheusnagot@hotmail.com>
2022-02-11 19:23:25 -0300

    refactor: "freelancer" using new private fn to calculate

--- codes/freelancer.ex
+++ codes/freelancer.ex
@@ -1,23 +1,29 @@
 defmodule FreelancerRates do
-  def daily_rate(hourly_rate) do
-    hourly_rate * 8.0
-  end
+  @daily_rate 8.0
+  @monthly_billable_days 22
+
+  def daily_rate(hourly_rate), do: hourly_rate * @daily_rate
 
   def apply_discount(before_discount, discount) do
     before_discount - before_discount * (discount / 100.0)
   end
 
-  def monthly_rate(hourly_rate, discount) do
-    total_rate = daily_rate(hourly_rate * 22)
+  defp daily_discounted_rate(hourly_rate, discount \\ 0) do
+    hourly_rate
+    |> daily_rate()
     |> apply_discount(discount)
-    |> Float.ceil()
-    |> trunc()
+  end
+
+  def monthly_rate(hourly_rate, discount) do
+    total_rate =
+      daily_rate(hourly_rate * @monthly_billable_days)
+      |> apply_discount(discount)
+      |> Float.ceil()
+      |> trunc()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    total_discount = budget * (discount / 100.0)
-
-    (budget / (daily_rate(hourly_rate) - total_discount))
+    (budget / daily_discounted_rate(hourly_rate, discount))
     |> Float.floor(1)
   end
 end
