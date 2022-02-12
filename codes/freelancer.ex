315ae1aa2cd1a44b2fe723c20855c845077e1a7b
Matheus <matheusnagot@hotmail.com>
2022-02-12 15:04:51 -0300

    refactor: change in organization of private fn and removal of unused var

--- codes/freelancer.ex
+++ codes/freelancer.ex
@@ -1,5 +1,6 @@
 defmodule FreelancerRates do
   @daily_rate 8.0
+
   @monthly_billable_days 22
 
   def daily_rate(hourly_rate), do: hourly_rate * @daily_rate
@@ -8,16 +9,16 @@ defmodule FreelancerRates do
     before_discount - before_discount * (discount / 100.0)
   end
 
+  def monthly_rate(hourly_rate, discount) do
+    daily_rate(hourly_rate * @monthly_billable_days)
+    |> apply_discount(discount)
+    |> ceil()
+  end
+
   defp daily_discounted_rate(hourly_rate, discount \\ 0) do
     hourly_rate
     |> daily_rate()
     |> apply_discount(discount)
-  end
-
-  def monthly_rate(hourly_rate, discount) do
-      total_rate = daily_rate(hourly_rate * @monthly_billable_days)
-      |> apply_discount(discount)
-      |> ceil()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
