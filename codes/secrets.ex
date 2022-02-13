4f01944eaac898bb623de06358610f99c40d8eb7
Matheus <matheusnagot@hotmail.com>
2022-02-13 10:46:57 -0300

    refactor: names of params in fn secrets

--- codes/secrets.ex
+++ codes/secrets.ex
@@ -1,32 +1,34 @@
 defmodule Secrets do
+  use Bitwise
+
   @moduledoc """
   This is a module.
   """
-  use Bitwise
+
   @type secret :: integer
 
   def secret_add(secret) do
-    fn param -> param + secret end
+    fn add_param -> add_param + secret end
   end
 
   def secret_subtract(secret) do
-    fn param -> param - secret end
+    fn subtract_param -> subtract_param - secret end
   end
 
   def secret_multiply(secret) do
-    fn param -> param * secret end
+    fn multiply_param -> multiply_param * secret end
   end
 
   def secret_divide(secret) do
-    fn param -> div(param, secret) end
+    fn divide_param -> div(divide_param, secret) end
   end
 
   def secret_and(secret) do
-    fn param -> param &&& secret end
+    fn and_param -> and_param &&& secret end
   end
 
   def secret_xor(secret) do
-    fn param -> bxor(param, secret) end
+    fn xor_param -> bxor(xor_param, secret) end
   end
 
   def secret_combine(secret_function1, secret_function2) do
