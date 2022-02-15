5be8bc66baa666e9965c04a959e748949719e39e
Matheus <matheusnagot@hotmail.com>
2022-02-15 20:18:35 -0300

    feat: new fn show task list

--- codes/task_list.ex
+++ codes/task_list.ex
@@ -2,8 +2,11 @@ defmodule TaskList do
   @moduledoc """
   Create a task list, read and write a file
   """
+
   def add(task_name) do
     task = "[ ]" <> task_name <> "\n"
-    File.write("task_list.md",task,[:append])
+    File.write("task_list.md", task, [:append])
   end
+
+  def show_list, do: File.read("task_list.md")
 end
