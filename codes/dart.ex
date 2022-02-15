5b23e825e3cec3fff9ab596ad9fc40ef77b2e0af
Matheus <matheusnagot@hotmail.com>
2022-02-14 21:35:17 -0300

    dart implementation

--- codes/dart.ex
+++ codes/dart.ex
@@ -6,5 +6,15 @@ defmodule Darts do
   """
   @spec score(position :: position) :: integer
   def score({x, y}) do
+    total_value_square = {x * x, y * y}
+      |> IO.inspect()
+      |> Tuple.sum()
+
+    cond do
+      total_value_square > 100 -> 0
+      total_value_square > 25 -> 1
+      total_value_square > 1 -> 5
+      true -> 10
+    end
   end
 end
