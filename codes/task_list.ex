3fb4788f69280afc0ca8f715ece0945bb9ae5619
Matheus <matheusnagot@hotmail.com>
2022-02-15 20:14:27 -0300

    doc: create a simple module doc

--- codes/task_list.ex
+++ codes/task_list.ex
@@ -1,4 +1,7 @@
 defmodule TaskList do
+  @moduledoc """
+  Create a task list, read and write a file
+  """
   def add(task_name) do
     task = "[ ]" <> task_name <> "\n"
     File.write("task_list.md",task,[:append])
