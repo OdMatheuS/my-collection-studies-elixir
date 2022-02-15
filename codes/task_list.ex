33b56b7bc1971732cad79d1784283969cde34436
Matheus <matheusnagot@hotmail.com>
2022-02-15 20:21:32 -0300

    refactor: using module attribute

--- codes/task_list.ex
+++ codes/task_list.ex
@@ -2,11 +2,12 @@ defmodule TaskList do
   @moduledoc """
   Create a task list, read and write a file
   """
+  @file_name "task_list.md"
 
   def add(task_name) do
     task = "[ ]" <> task_name <> "\n"
-    File.write("task_list.md", task, [:append])
+    File.write(@file_name, task, [:append])
   end
 
-  def show_list, do: File.read("task_list.md")
+  def show_list, do: File.read(@file_name)
 end
