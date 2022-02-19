e599ae2a6fb548a35f425a28d9dc3c5262b534d1
Matheus <matheusnagot@hotmail.com>
2022-02-19 11:33:20 -0300

    refactor: using strictly equal operator

--- codes/guess_game_1.ex
+++ codes/guess_game_1.ex
@@ -10,7 +10,7 @@ defmodule GuessingGame do
   def compare(secret_number, guess \\ 0)
   def compare(_secret_number, guess) when guess === 0 or is_atom(guess), do: "Make a guess"
   def compare(secret_number, guess) when secret_number === guess, do: "Correct"
-  def compare(secret_number, guess) when abs(secret_number - guess) == 1, do: "So close"
+  def compare(secret_number, guess) when abs(secret_number - guess) === 1, do: "So close"
   def compare(secret_number, guess) when secret_number < guess, do: "Too high"
   def compare(secret_number, guess) when secret_number > guess, do: "Too low"
 end
