fc9da3d998fa62402d967d0923857dd3760bbfc6
Matheus <matheusnagot@hotmail.com>
2022-02-19 11:12:25 -0300

    feat: add fn when the guess is one more or one less than the secret number

--- codes/guess_game_1.ex
+++ codes/guess_game_1.ex
@@ -1,10 +1,11 @@
 defmodule GuessingGame do
-@moduledoc """
-creating a trivial game where you have to guess a secret number.
-using functions for detailed feedback on how the guess relates to the secret number.
-"""
+  @moduledoc """
+  creating a trivial game where you have to guess a secret number.
+  using functions for detailed feedback on how the guess relates to the secret number.
+  """
   def compare(_secret_number, guess \\ 0) when guess === 0 or is_atom(guess), do: "Make a guess"
   def compare(secret_number, guess) when secret_number === guess, do: "Correct"
+  def compare(secret_number, guess) when abs(secret_number - guess) == 1, do: "So close"
   def compare(secret_number, guess) when secret_number < guess, do: "Too high"
   def compare(secret_number, guess) when secret_number > guess, do: "Too low"
 end
