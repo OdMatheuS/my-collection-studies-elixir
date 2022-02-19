6faf1bf7292a9344922aa8528bca81a2b24e8de9
Matheus <matheusnagot@hotmail.com>
2022-02-19 11:35:31 -0300

    refactor: change default value and guard

--- codes/guess_game_1.ex
+++ codes/guess_game_1.ex
@@ -7,8 +7,8 @@ defmodule GuessingGame do
   @type secret_number :: integer
   @type guess :: integer
 
-  def compare(secret_number, guess \\ 0)
-  def compare(_secret_number, guess) when guess === 0 or is_atom(guess), do: "Make a guess"
+  def compare(secret_number, guess \\ :no_guess)
+  def compare(_secret_number, guess) when is_atom(guess), do: "Make a guess"
   def compare(secret_number, guess) when secret_number === guess, do: "Correct"
   def compare(secret_number, guess) when abs(secret_number - guess) === 1, do: "So close"
   def compare(secret_number, guess) when secret_number < guess, do: "Too high"
