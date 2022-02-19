4b8e2d73eb20b315ad31e4117ff7fc55e38e2074
Matheus <matheusnagot@hotmail.com>
2022-02-19 11:19:14 -0300

    refactor: default values must be defined in a header

--- codes/guess_game_1.ex
+++ codes/guess_game_1.ex
@@ -3,7 +3,8 @@ defmodule GuessingGame do
   creating a trivial game where you have to guess a secret number.
   using functions for detailed feedback on how the guess relates to the secret number.
   """
-  def compare(_secret_number, guess \\ 0) when guess === 0 or is_atom(guess), do: "Make a guess"
+  def compare(secret_number, guess \\ 0)
+  def compare(_secret_number, guess) when guess === 0 or is_atom(guess), do: "Make a guess"
   def compare(secret_number, guess) when secret_number === guess, do: "Correct"
   def compare(secret_number, guess) when abs(secret_number - guess) == 1, do: "So close"
   def compare(secret_number, guess) when secret_number < guess, do: "Too high"
