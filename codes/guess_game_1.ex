4c23f3befbef1f4469eeb73514eacec2268df6e7
Matheus <matheusnagot@hotmail.com>
2022-02-19 11:20:03 -0300

    add types for parameters

--- codes/guess_game_1.ex
+++ codes/guess_game_1.ex
@@ -3,6 +3,10 @@ defmodule GuessingGame do
   creating a trivial game where you have to guess a secret number.
   using functions for detailed feedback on how the guess relates to the secret number.
   """
+
+  @type secret_number :: integer
+  @type guess :: integer
+
   def compare(secret_number, guess \\ 0)
   def compare(_secret_number, guess) when guess === 0 or is_atom(guess), do: "Make a guess"
   def compare(secret_number, guess) when secret_number === guess, do: "Correct"
