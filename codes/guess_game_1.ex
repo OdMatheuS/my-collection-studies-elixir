5f8a08750b832d38d6ae64768b3b17d09f7de9ba
Matheus <matheusnagot@hotmail.com>
2022-02-19 11:07:11 -0300

    add new code Guessing Game - Exercism

---
+++ codes/guess_game_1.ex
@@ -0,0 +1,10 @@
+defmodule GuessingGame do
+@moduledoc """
+creating a trivial game where you have to guess a secret number.
+using functions for detailed feedback on how the guess relates to the secret number.
+"""
+  def compare(_secret_number, guess \\ 0) when guess === 0 or is_atom(guess), do: "Make a guess"
+  def compare(secret_number, guess) when secret_number === guess, do: "Correct"
+  def compare(secret_number, guess) when secret_number < guess, do: "Too high"
+  def compare(secret_number, guess) when secret_number > guess, do: "Too low"
+end
