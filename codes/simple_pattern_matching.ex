fa1b3b56dc02fef22838ae264482a46cd8a35644
Matheus <matheusnagot@hotmail.com>
2022-02-23 22:24:57 -0300

    add: a little more about pattern matching

---
+++ codes/simple_pattern_matching.ex
@@ -0,0 +1,10 @@
+# studying a little more patter matching
+
+# Matching parts of string
+"Deku:" <> get_name_hero = "Deku: Izuku Midoriya "
+
+"Best-Number-1:" <> best_hero_number_one = "Best-Number-1: All Might"
+
+"Kamado" <> name_best_brother = "Kamado Tanjiro"
+
+"Water:" <> name_hashira_of_water = "Water:Giyu Tomioka"
