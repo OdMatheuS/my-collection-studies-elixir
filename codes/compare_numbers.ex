da12a389ada8521a3c1c3347aea79cd3d5845f53
Matheus <matheusnagot@hotmail.com>
2022-02-28 22:08:56 -0300

    feat: implementing check and greater_than function

--- codes/compare_numbers.ex
+++ codes/compare_numbers.ex
@@ -1,2 +1,17 @@
+# this module, was one of the great lessons I learned in
+# the book "Learn Functional Programming with Elixir: New Foundations for a New World" by Ulisses Almeida,
+# it shows in a very simple way how to implement a comparison of numbers
+
 defmodule CompareNumbers do
+  def greater_than(first_number, second_number) do
+    check(first_number >= second_number, first_number, second_number)
+  end
+
+  defp check(true, number, _) do
+    number
+  end
+
+  defp check(false, _, other_number) do
+    other_number
+  end
 end
