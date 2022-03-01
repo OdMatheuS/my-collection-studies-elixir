d3b8ba694510bc88ac25c920f6fe66fa0d9687ac
Matheus <matheusnagot@hotmail.com>
2022-03-01 19:18:55 -0300

    style: format code

--- codes/leap_year.ex
+++ codes/leap_year.ex
@@ -11,7 +11,7 @@ defmodule Year do
   @spec leap_year?(non_neg_integer) :: boolean
   def leap_year?(year) do
     cond do
-      rem(year,400) == 0 -> true
+      rem(year, 400) == 0 -> true
       rem(year, 100) == 0 -> false
       rem(year, 4) == 0 -> true
       true -> false
