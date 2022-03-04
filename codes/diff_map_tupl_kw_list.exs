e14e0208af488fdd7181a8c43b2d0d874dc728a9
Matheus <matheusnagot@hotmail.com>
2022-03-04 16:26:50 -0300

    feat: some Tuple examples

--- codes/diff_map_tupl_kw_list.exs
+++ codes/diff_map_tupl_kw_list.exs
@@ -19,3 +19,11 @@ katsuki_bakugo = %{"age" => 17}
   {:hero, "All Might"} => "Tuple",
   %{only_key: true} => "HI! Map",
 }
+
+# ---------------------------------------------------
+# Tuple ---------------------------------------------
+tuple = {:inosuke, :pig, 7, "Assault"}
+get_elem(tuple, 1) # return :pig
+
+# Tuple example with multiple types of data.
+{"Giyu Tomioka", 0101, :abc, false}
