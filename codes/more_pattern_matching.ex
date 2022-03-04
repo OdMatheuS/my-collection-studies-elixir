296b906a40612432fc7f03af25925547d63eb7ed
Matheus <matheusnagot@hotmail.com>
2022-03-03 22:33:04 -0300

    add: new code, simple review pattern matching

---
+++ codes/more_pattern_matching.ex
@@ -0,0 +1,9 @@
+# simple script to review pattern matching
+heros_info = [
+  %{name: "Toshinori Yagi", gender: "Male", hero_name: "All Might"},
+  %{name: "Fumikage Tokoyami", gender: "Male", hero_name: "Tsukuyomi"},
+  %{name: "Toru Hagakure", gender: "Female", hero_name: "Invisible Girl"},
+  %{name: "Tsuyu Asui", gender: "Female", hero_name: "Froppy"},
+  %{name: "Shoto Todoroki", gender: "Male", hero_name: "Shoto"},
+  %{name: "Ochaco Uraraka", gender: "Female", hero_name: "Uravity"},
+]
