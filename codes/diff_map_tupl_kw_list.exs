9e02f1c040519fe713e51253a75ba41aa9a71dea
Matheus <matheusnagot@hotmail.com>
2022-03-04 16:24:47 -0300

    feat: some Map examples

---
+++ codes/diff_map_tupl_kw_list.exs
@@ -0,0 +1,21 @@
+# -------------------------------------------------
+# Map ---------------------------------------------
+izuku_midoriya = %{"age" => 16}
+katsuki_bakugo = %{"age" => 17}
+
+# String-Key Version
+%{"hero_name" => "Endeavor", "age" => 46}
+
+# Atom-Key Version
+%{dabi: "Villain", age: 24}
+
+# Other Key Types
+# A map can have any data structure as a key and a value.
+%{
+  true => "trueval/boolean key",
+  8 => "Integer",
+  7.5 => "is a Float",
+  [1, 2, 3] => "simple list"
+  {:hero, "All Might"} => "Tuple",
+  %{only_key: true} => "HI! Map",
+}
