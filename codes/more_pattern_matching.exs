7a9b0c75b06e057d4b97ba6caa5294708d3d7e42
Matheus <matheusnagot@hotmail.com>
2022-03-03 22:38:56 -0300

    refactor: changing the extension to a script file and rename variable

--- codes/more_pattern_matching.exs
+++ codes/more_pattern_matching.exs
@@ -1,5 +1,5 @@
 # simple script to review pattern matching
-heros_info = [
+heros_infos = [
   %{name: "Toshinori Yagi", gender: "Male", hero_name: "All Might"},
   %{name: "Fumikage Tokoyami", gender: "Male", hero_name: "Tsukuyomi"},
   %{name: "Toru Hagakure", gender: "Female", hero_name: "Invisible Girl"},
@@ -7,3 +7,5 @@ heros_info = [
   %{name: "Shoto Todoroki", gender: "Male", hero_name: "Shoto"},
   %{name: "Ochaco Uraraka", gender: "Female", hero_name: "Uravity"},
 ]
+
+[first_hero, second_hero, third_hero | others_heros] = heros_infos
