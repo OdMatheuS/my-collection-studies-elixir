6e016be715436ee9c905b6add6c1f4482d0e39cf
Matheus <matheusnagot@hotmail.com>
2022-03-05 19:51:10 -0300

    add: new module, KitchenCalculator

---
+++ codes/kitchen_calculator.ex
@@ -0,0 +1,13 @@
+defmodule KitchenCalculator do
+  def get_volume(volume_pair) do
+  end
+
+  def to_milliliter(volume_pair) do
+  end
+
+  def from_milliliter(volume_pair, unit) do
+  end
+
+  def convert(volume_pair, unit) do
+  end
+end
