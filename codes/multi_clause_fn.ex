5ab30c25de39125e056c855db5527a4691a68c9c
Matheus <matheusnagot@hotmail.com>
2022-03-07 23:54:36 -0300

    feat: implementing a new fn

--- codes/multi_clause_fn.ex
+++ codes/multi_clause_fn.ex
@@ -3,6 +3,10 @@ defmodule MultipleClause do
   def get_info_kimetsu(:tanjiro = best_brother) do
     {best_brother, "Tanjiro Kamado"}
   end
+
+  def get_info_kimetsu(:pig = hunter_nickname) do
+    {hunter_nickname, "Inosuke Hashibira"}
+  end
 end
 
 MultipleClause.get_info_kimetsu(:tanjiro)
