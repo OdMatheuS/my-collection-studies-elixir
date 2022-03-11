80ca0e430fbedd56eaab372e4ccdf51646061038
Matheus <matheusnagot@hotmail.com>
2022-03-11 00:05:54 -0300

    doc: adding description and reference from official Elixir documentation

--- codes/strings.ex
+++ codes/strings.ex
@@ -1,1 +1,5 @@
+# following the introduction of strings in the Elixir School documentation ; link -> https://elixirschool.com/en/lessons/basics/strings#strings-0
+
+# "Elixir strings are nothing but a sequence of bytes."
+# understanding this concept I can create a text, using its reference in ASCII Code
 best_brother = <<116,97,110,106,105,114,111,32,107,97,109,97,100,111>>
