eca2bf6d3ea9c21f7d7798cd69f1fdea3c304dd6
Matheus <matheusnagot@hotmail.com>
2022-03-13 19:30:06 -0300

    feat: learning about character code point using "?"

--- codes/strings.ex
+++ codes/strings.ex
@@ -3,3 +3,10 @@
 # "Elixir strings are nothing but a sequence of bytes."
 # understanding this concept I can create a text, using its reference in ASCII Code
 best_brother = <<116,97,110,106,105,114,111,32,107,97,109,97,100,111>>
+
+# find the character code point using "?"
+?t
+# return -> 16
+
+list_char_code_point = [ ?t,?a, ?n, ?j, ?i, ?r, ?o, ]
+# return -> 'tanjiro'
