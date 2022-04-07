879da748046ae0b758f0af832f2415333f71276d
Matheus N <56494951+OdMatheuS@users.noreply.github.com>
2022-04-07 14:47:59 -0300

    doc: little note about function initials()

--- codes/high_school_sweetheart.ex
+++ codes/high_school_sweetheart.ex
@@ -1,3 +1,15 @@
+"""
+note: me from the future read this
+
+in the third fn, could have used a shorter expression, instead of that Enum.map(),
+but currently I found it clearer to use that more verbose way,
+maybe in the future I'll see this and want to refactor as soon as possible LOL
+
+    |> Enum.map(& initial/1)
+
+    |> Enum.map(& initial(&1))
+"""
+
 defmodule HighSchoolSweetheart do
   def first_letter(name) do
     name
