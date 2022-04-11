2d7e96903cd66d7158f6ceb5fc744238cd4a800f
Matheus N <56494951+OdMatheuS@users.noreply.github.com>
2022-04-11 17:01:27 -0300

    refactor:add module attributes, for default score

--- codes/high_score.ex
+++ codes/high_score.ex
@@ -1,10 +1,12 @@
 defmodule HighScore do
+  
+  @default_score 0
 
   def new() do
     Map.new()
   end
 
-  def add_player(scores, name, score \\ 0) do
+  def add_player(scores, name, score \\ @default_score) do
     Map.put(scores, name, score)
   end
 
