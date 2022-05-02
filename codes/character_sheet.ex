5b20dded4ff2a44329dcc87280dfaa235fcaa322
Matheus <56494951+OdMatheuS@users.noreply.github.com>
2022-05-02 15:59:50 -0300

    feat:implemented fn ask_name()

--- codes/character_sheet.ex
+++ codes/character_sheet.ex
@@ -1,3 +1,9 @@
 defmodule RPG.CharacterSheet do
-    def welcome(), do: IO.puts "Welcome! Let's fill out your character sheet together."
+  def welcome(), do: IO.puts "Welcome! Let's fill out your character sheet together."
+    
+  def ask_name() do
+    IO.gets("What is your character's name?\n")
+    |> String.trim()
+  end
+  
 end
