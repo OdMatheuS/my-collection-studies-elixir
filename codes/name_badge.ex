e05de49489e2013a4d39366a4a82c3f64b981eeb
Matheus <56494951+OdMatheuS@users.noreply.github.com>
2022-05-09 14:01:14 -0300

    feat:implemented fn print(), print a badge for an employee

--- codes/name_badge.ex
+++ codes/name_badge.ex
@@ -1,2 +1,9 @@
 defmodule NameBadge do
+  def print(id, name, department) do
+    if id != nil do
+      "[#{id}] - #{name} - #{String.upcase(department)}"
+    else
+      "#{name} - #{String.upcase(department)}"
+    end
+  end
 end
