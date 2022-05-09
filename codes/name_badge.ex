473a2bc7030a545be80e71543d4d6f51217b5919
Matheus <56494951+OdMatheuS@users.noreply.github.com>
2022-05-09 14:55:15 -0300

    feat:implemented fn print to all cases tested

--- codes/name_badge.ex
+++ codes/name_badge.ex
@@ -1,9 +1,9 @@
 defmodule NameBadge do
   def print(id, name, department) do
-    if id != nil do
-      "[#{id}] - #{name} - #{String.upcase(department)}"
+    if department == nil do
+      if id == nil, do: "#{name} - OWNER", else: "[#{id}] - #{name} - OWNER"
     else
-      "#{name} - #{String.upcase(department)}"
+      if id != nil, do: "[#{id}] - #{name} - #{String.upcase(department)}", else: "#{name} - #{String.upcase(department)}"
     end
   end
 end
