916bb9873a5424b9b49022394a1029c05a3a8c1e
Matheus <56494951+OdMatheuS@users.noreply.github.com>
2022-05-09 15:10:09 -0300

    docs:replace if to multiple clause functions

--- codes/name_badge.ex
+++ codes/name_badge.ex
@@ -7,3 +7,14 @@ defmodule NameBadge do
     end
   end
 end
+
+"""
+another way... using Multiple clause functions
+
+defmodule NameBadge do
+  def print(nil, name, nil), do: "#{name} - OWNER"
+  def print(nil, name, department), do: "#{name} - #{String.upcase(department)}"
+  def print(id, name, nil), do: "[#{id}] - #{name} - OWNER"
+  def print(id, name, department), do: "[#{id}] - #{name} - #{String.upcase(department)}"
+end
+"""
